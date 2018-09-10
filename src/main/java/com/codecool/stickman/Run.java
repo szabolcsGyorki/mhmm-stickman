b631d03a54fea1241acc974ef85b5728ae4364a9
Szabolcs Gyorki <sz.gyorki@gmail.com>
2018-09-10 15:05:19 +0200

    The base of the project seems to be working

--- src/main/java/com/codecool/stickman/Run.java
+++ src/main/java/com/codecool/stickman/Run.java
@@ -1,4 +1,26 @@
 package com.codecool.stickman;
 
-public class Run {
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "Test", urlPatterns = {"/"})
+public class Run extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        PrintWriter out = response.getWriter();
+        out.println(
+                "<html>\n" +
+                "<head><title>Another page</title></head>\n" +
+                "<body>\n" +
+                "<h1>Hello CodeCooler!</h1>" +
+                "<br/>" +
+                "<div><a href=\"/\">Back to home</a></div>" +
+                "</body></html>"
+        );
+    }
 }
