7580aad764cfdfd8c3d8b69b597395f94fb2889a
mvotir <38725668+mvotir@users.noreply.github.com>
2018-09-11 09:45:15 +0200

    Game Objects "imported"

---
+++ src/main/java/com/codecool/stickman/GameObjects/Items/Item.java
@@ -0,0 +1,39 @@
+package com.codecool.stickman.GameObjects.Items;
+
+import com.codecool.stickman.GameObjects.Characters.Character;
+import com.codecool.stickman.GameObjects.GameObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public abstract class Item extends GameObject {
+
+    private int value;
+    private int id;
+    private static int nextId;
+    private String name;
+    private static final Logger logger = LoggerFactory.getLogger(Item.class);
+
+    Item(String name, int value) {
+        super(0,0);
+        this.value = value;
+        this.name = name;
+        this.id = nextId++;
+    }
+
+    void assignToCharacter(Character character) {
+        character.addItemToInventory(this);
+        logger.info("'{}' added to inventory", this.name);
+    };
+
+    public int getValue() {
+        return value;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    String getName() {
+        return name;
+    }
+}
