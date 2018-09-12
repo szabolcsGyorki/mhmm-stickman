98cec48607cb2d702beb0456d0fa689a4b289b6f
Tibi <bukkfabiszti@gmail.com>
2018-09-12 09:51:04 +0200

    Commit before push

--- src/main/webapp/static/js/display.js
+++ src/main/webapp/static/js/display.js
@@ -44,10 +44,11 @@ function ajax_get(url, callback) {
         }
     };
     xmlhttp.open("GET", url, true);
-    xmlhttp.send();
+    xmlhttp.send('map');
 
 }
 
+//ajax test bound to a button temp.
 let button = document.getElementById('test');
 button.onclick = function () {
 
@@ -55,22 +56,39 @@ button.onclick = function () {
         for (x in data) {
             for (y in data[x]) {
                 switch (data[x][y].name) {
-                    case 'DRAGON': image(dragon_image);
+                    case 'DRAGON': image(dragon_image, x*50, y*50, height/12, width/12);
                         break;
-                    case 'LOOT': image(loot_image);
+                    case 'LOOT': image(loot_image, x*50, y*50, height/12, width/12);
                         break;
-                    case 'MAIN_CHARACTER': image(main_character_image);
+                    case 'MAIN_CHARACTER': image(main_character_image, x*50, y*50, height/12, width/12);
                         break;
-                    case 'SKELETON': image(skeleton_image);
+                    case 'SKELETON': image(skeleton_image, x*50, y*50, height/12, width/12);
                         break;
-                    case 'SLIME': image(slime_image);
+                    case 'SLIME': image(slime_image, x*50, y*50, height/12, width/12);
                         break;
-                    case 'WALL': image(wall_image);
+                    case 'WALL': image(wall_image, x*50, y*50, height/12, width/12);
                         break;
                 }
             }
         }
     })
+};
 
 
+//arrow handlers
+document.onkeydown = function(e) {
+    switch (e.key) {
+        case 37: //left
+            alert('left');
+            break;
+        case 39: //right
+            alert('right');
+            break;
+        case 38: //up
+            alert('up');
+            break;
+        case 40: //down
+            alert('down');
+            break;
+    }
 };
